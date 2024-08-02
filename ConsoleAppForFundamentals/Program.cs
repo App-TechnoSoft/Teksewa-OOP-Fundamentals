878360e6cb34d93e836402582d36ed1d479dc1f4
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-08-02 09:41:53 +0545

    Learn about datatypes

--- ConsoleAppForFundamentals/Program.cs
+++ ConsoleAppForFundamentals/Program.cs
@@ -1,2 +1,35 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿// Datatypes
+// Numbers: Integers and Floating points
+
+byte? age = 34;
+short numberOfPeopeInCompany = 23424;   // Camel casing
+int areaOfCountry = 234224424;
+long distanceBetweenPlanets = 234223252353;
+
+// Accuracy (Precision)
+float averageWeight = 67.5342324242424f;
+double averageDistance = 23422.34224242234234224;
+decimal averageDistanceBetweenElectrons = 23422.34224242234234224333253m;
+
+// Text
+string name = "Bishnu Ji";
+char gender = 'M';
+
+// Flags
+bool isQA = true;
+
+// Dates
+DateTime dob = new(1990, 12, 21);
+
+// Nullables
+
+//Console.WriteLine(averageWeight);
+
+string userInput = Console.ReadLine();
+Console.WriteLine($"Your entered {userInput.Length} characters");
+
+// Get user name and convert it to uppercase
+
+
+
+
